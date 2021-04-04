fa36bab9cc17acd4a8a84e213b4aa3a9d9fa5393
legoffchristophealexandre@gmail.com <legoffchristophealexandre@gmail.com>
2021-04-04 23:47:19 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import { Header, MovieList} from "./components";
+import { Header, MovieList } from "./components";
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App d-flex flex-column">
-      <Header />
-      <div className="d-flex flex-row flex-fill pt-4 p-2">
-        <MovieList />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [
+        {
+        title: 'The Godfather',
+        img: 'https://fr.web.img6.acsta.net/pictures/20/10/02/12/21/3764004.png',
+        details: 'R | 175 min | Crime, Drama',
+        description: 'La conclusion de la saga Skywalker. De nouvelles légendes vont naître dans cette bataille épique pour la liberté.'
+      },
+      {
+        title: 'The Godfather',
+        img: 'https://fr.web.img6.acsta.net/pictures/20/10/02/12/21/3764004.png',
+        details: 'R | 175 min | Crime, Drama',
+        description: 'La conclusion de la saga Skywalker. De nouvelles légendes vont naître dans cette bataille épique pour la liberté.'
+      },
+      {
+        title: 'The Godfather',
+        img: 'https://fr.web.img6.acsta.net/pictures/20/10/02/12/21/3764004.png',
+        details: 'R | 175 min | Crime, Drama',
+        description: 'La conclusion de la saga Skywalker. De nouvelles légendes vont naître dans cette bataille épique pour la liberté.'
+      },
+      {
+        title: 'The Godfather',
+        img: 'https://fr.web.img6.acsta.net/pictures/20/10/02/12/21/3764004.pnghttps://fr.web.img6.acsta.net/pictures/20/10/02/12/21/3764004.pnghttps://fr.web.img6.acsta.net/pictures/20/10/02/12/21/3764004.pnghttps://fr.web.img6.acsta.net/pictures/20/10/02/12/21/3764004.pnghttps://fr.web.img6.acsta.net/pictures/20/10/02/12/21/3764004.png',
+        details: 'R | 175 min | Crime, Drama',
+        description: 'La conclusion de la saga Skywalker. De nouvelles légendes vont naître dans cette bataille épique pour la liberté.'
+      }
+    ],
+      selectedMovie: 0
+    }
+  }
+  render() {
+    return (
+      <div className="App d-flex flex-column">
+        <Header />
+        <div className="d-flex flex-row flex-fill pt-4 p-2">
+          <MovieList />
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
--- src/components/movie-list/MovieElement.js
+++ src/components/movie-list/MovieElement.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 class MovieElement extends Component {
     render() {
         return (
-            <div className="w-25 p-2">
-                <div className="card">
-                    <img src="https://fr.web.img6.acsta.net/pictures/20/10/02/12/21/3764004.png" alt="Star Wras" className="card-img-top"/>
-                    <div className="card-body">
-                        <h5 className="card-title">Star Wars</h5>
-                        <p classname="card-text">La conclusion de la saga Skywalker. De nouvelles légendes vont naître dans cette bataille épique pour la liberté.</p>
+            <div className="w-50 p-2">
+                <div className="border d-flex">
+                    <img width="150" height="200" src="https://fr.web.img6.acsta.net/pictures/20/10/02/12/21/3764004.png" alt="Star Wras" />
+                    <div className="flex-fill d-flex flex-column p-3">
+                        <h5>Star Wars</h5>
+                        <hr className="w-100"></hr>
+                        <p>La conclusion de la saga Skywalker. De nouvelles légendes vont naître dans cette bataille épique pour la liberté.</p>
                     </div>
                 </div>
             </div>
