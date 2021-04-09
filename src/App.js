802aec589808905083235d3d5a601f3660b6aec2
legoffchristophealexandre@gmail.com <legoffchristophealexandre@gmail.com>
2021-04-09 14:30:20 +0200

    SearchBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Header, MovieDetails, MovieList, Loading } from "./components";
+import { Header, MovieDetails, MovieList, Loading, SearchBar } from "./components";
 import { Component } from 'react';
-import apiMovie from './conf/api.movie'
+import apiMovie, { apiMovieMap } from './conf/api.movie'
 
 class App extends Component {
   constructor(props) {
@@ -22,18 +22,13 @@ class App extends Component {
     apiMovie.get('/discover/movie')
             .then( response => response.data.results)
             .then( apiMovie => {
-              const movies = apiMovie.map(m => ({
-                img: 'https://image.tmdb.org/t/p/w500' + m.poster_path,
-                title: m.title,
-                details: `${ m.release_date } | ${ m.vote_average }/10 (${ m.vote_count })`,
-                description: m.overview
-              }))
+              const movies = apiMovie.map(apiMovieMap)
               this.updateMovies(movies)
             })
             .catch( err => console.log(err))
   }
 
-  updateMovies(movies) {
+  updateMovies = (movies) => {
     this.setState({
       movies,
       loaded: true
@@ -44,7 +39,7 @@ class App extends Component {
     return (
       <div className="App d-flex flex-column">
         <Header />
-
+        <SearchBar updateMovies = { this.updateMovies } />
         { this.state.loaded ? (
           <div className="d-flex flex-row flex-fill pt-4 p-2">
             <MovieList movies={this.state.movies} updateSelectedMovie={this.updateSelectedMovie} />
---
+++ src/components/search-bar/SearchBar.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Formik } from 'formik';
+import apiMovie, { apiMovieMap } from './../../conf/api.movie';
+
+export default class SearchBar extends Component {
+
+    submit = (values, actions) => {
+        // console.log(values);
+        const query = '?' + Object.keys(values).map(k => `${ k }=${ values[k]}&`).join('')
+        apiMovie.get('/search/movie' + query)
+            .then( response => response.data.results)
+            .then( apiMovie => {
+            const movies = apiMovie.map(apiMovieMap)
+            this.props.updateMovies(movies)
+            actions.setSubmitting(false)
+            })
+            .catch( err => console.log(err))
+    }
+
+    render() {
+        return (
+            <Formik onSubmit={ this.submit }
+                initialValues = {{
+                    query: '',
+                    language: 'en-US'
+                }}
+            >
+                { ({ handleBlur, handleChange, handleSubmit, isSubmitting }) => (
+                    <form className="d-flex flex-row m-2 p-2" onSubmit={ handleSubmit }>
+                        <input name="query" className="flex-fill form-control mr-2" placeholder="Chercher un film ..." onChange={ handleChange } onBlur={ handleBlur }></input>
+                        <select className="mr-2 form-control w-25" name="language" onChange={ handleChange } onBlur={ handleBlur }>
+                            <option value="en-US"> Anglais </option>
+                            <option value="fr-FR"> Français </option>
+                        </select>
+                        <button className="btn btn-small btn-success" type="submit" disabled={ isSubmitting }>Rechercher</button>
+                    </form>
+                ) }
+            </Formik>
+        )       
+    }
+}
