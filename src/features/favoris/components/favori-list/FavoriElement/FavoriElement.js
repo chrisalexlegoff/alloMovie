19c324bdaaa380a634e6d9a0ff0247b473ceebf5
legoffchristophealexandre@gmail.com <legoffchristophealexandre@gmail.com>
2021-04-12 08:28:09 +0200

    update Favoris

--- src/features/favoris/components/favori-list/FavoriElement/FavoriElement.js
+++ src/features/favoris/components/favori-list/FavoriElement/FavoriElement.js
@@ -1,23 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Style from './FavoriElement.module.scss'
+import { useHistory } from 'react-router-dom';
 
-class FavoriElement extends Component {
+function FavoriElement(props) {
 
-    render() {
-        return (
-            <div className={"d-flex flex-row bg-light " + Style.container}>
-                <img width="185" src={this.props.favori.img} alt="film" />
+    let history = useHistory();
+
+    function handleClick() {
+        history.push('/detail');
+    }
+      
+    return (
+      
+        <div className={"d-flex flex-row bg-light " + Style.container}>
+                <img width="185" src={props.favori.img} alt="film" />
                 <div className="flex-fill d-flex flex-column p-3">
-                    <h5>{this.props.favori.title}</h5>
+                    <h5>{props.favori.title}</h5>
                     <hr className="w-100"></hr>
-                    <p className="flex-fill">{this.props.favori.details}</p>
+                    <p className="flex-fill">{props.favori.details}</p>
                     <div className="d-flex flex-row justify-content-end">
-                        <button style={{fontSize: 'xx-small'}} onClick={ () => { this.props.removeFavori(this.props.favori.title) } } className="btn btn-small btn-danger"> Retirer des favoris </button>
+                        <button style={{fontSize: 'x-small'}} onClick={ () => { props.removeFavori(props.favori.title) } } className="btn btn-small btn-danger"> Retirer des favoris </button>
+                    
+                        <button style={{fontSize: 'x-small'}} onClick={ () => { handleClick() } } className="btn btn-small btn-success ml-2"> Voir les détails </button>
                     </div>
                 </div>
-            </div>
-        );
-    }
+            </div> )
 }
+
 
 export default FavoriElement;
--- src/features/favoris/components/favori-list/FavoriList.js
+++ src/features/favoris/components/favori-list/FavoriList.js
@@ -1,20 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 import FavoriElement from './FavoriElement/FavoriElement';
+import Sorry from '../../../../asset/sorry.jpg'
+import { useHistory } from 'react-router-dom';
 
-class FavoriList extends Component {
-    render() {
-        return (
-            <div className="w-75 d-flex flex-row flex-wrap justify-content-center">
-                { this.props.favoris.map( (f, index) => (
-                    <FavoriElement 
-                        key={ f.title + index }
-                        favori={ f } 
-                        removeFavori={ this.props.removeFavori }
-                    />
-                )) }
-            </div>
-        );
+
+function FavoriList(props) {
+
+    let history = useHistory();
+
+    function handleClick() {
+        history.goBack();
     }
+      console.log(props.favoris.length)
+    return (
+      
+        <>
+                <div className="d-flex flex-column align-items-center">
+                    <button className="w-25 btn btn-small btn-primary mb-4" onClick={ () => handleClick() }> Retour </button>
+                
+        
+        { props.favoris.length > 0 ? (
+           
+                <div className="d-flex flex-row flex-wrap justify-content-center">
+            { props.favoris.map( (f, index) => (
+                <FavoriElement 
+                key={ f.title + index }
+                favori={ f } 
+                removeFavori={ props.removeFavori }
+                />
+            )) }
+        </div>
+            
+        
+    ) : (
+        <div className="w-100 d-flex flex-row flex-wrap justify-content-center">
+            <h1>Vous n'avez pas encore ajouté de film dans vos favoris</h1>
+            <div ><img src={Sorry} alt="sorry"/></div>
+            
+        </div>
+        
+    ) }</div>
+        </>
+    )
 }
+
+
+
 
 export default FavoriList
